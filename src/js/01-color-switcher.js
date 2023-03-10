e492508b1494e380cfac5d289bd7318f5451ee3b
KuhaiAnna <kuhai.anna@gmail.com>
2023-03-10 17:12:16 +0200

    Add code for the task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
 
+let colorTimerId = null;
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick(e) {
+  e.currentTarget.disabled = true;
+  stopBtn.disabled = false;
+
+  colorTimerId = setInterval(() => {
+    const randomHexColor = getRandomHexColor();
+    bodyEl.style.backgroundColor = randomHexColor;
+  }, 1000);
+}
+
+function onStopBtnClick(e) {
+  e.currentTarget.disabled = true;
+  startBtn.disabled = false;
+
+  clearInterval(colorTimerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
