8811b6c1118f6d7aa72b2b505ab562d904d1535a
KuhaiAnna <kuhai.anna@gmail.com>
2023-03-13 22:49:08 +0200

    Update code for task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,12 +1,19 @@
+// Посмлання на елементи
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 const bodyEl = document.querySelector('body');
 
+// Кнопка stop неактивна по замовчуванню
+stopBtn.disabled = true;
+
+// Ідентифікатор setInterval
 let colorTimerId = null;
 
+// Слухачі подій
 startBtn.addEventListener('click', onStartBtnClick);
 stopBtn.addEventListener('click', onStopBtnClick);
 
+// Функція, що запускає зміну кольору фону
 function onStartBtnClick(e) {
   e.currentTarget.disabled = true;
   stopBtn.disabled = false;
@@ -17,6 +24,7 @@ function onStartBtnClick(e) {
   }, 1000);
 }
 
+// Функція, що зупиняє зміну кольору фону
 function onStopBtnClick(e) {
   e.currentTarget.disabled = true;
   startBtn.disabled = false;
@@ -24,6 +32,7 @@ function onStopBtnClick(e) {
   clearInterval(colorTimerId);
 }
 
+// Генерація випадкового кольору
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
