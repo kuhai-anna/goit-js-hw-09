4b3eef2bf60328fa361bf09097f2fe84fb2b2bae
KuhaiAnna <kuhai.anna@gmail.com>
2023-03-13 22:40:28 +0200

    Add code for task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,50 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+// Бібліотека для відображення повідомлень користувачеві
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// Посилання на елементи
+const formEl = document.querySelector('.form');
+const delayInput = document.querySelector('[name="delay"]');
+const stepInput = document.querySelector('[name="step"]');
+const amountInput = document.querySelector('[name="amount"]');
+const submitBtn = document.querySelector('[type="submit"]');
+
+// Слухач подій
+// submitBtn.addEventListener('submit', onSubmitBtnClick);
+submitBtn.addEventListener('click', onSubmitBtnClick);
+
+// Запуск createPromise по кліку
+function onSubmitBtnClick(e) {
+  e.preventDefault(); // Заборона перезавантаження сторінки
+
+  for (let i = 0; i < amountInput.value; i += 1) {
+    const position = i + 1;
+    const delay = Number(delayInput.value) + i * Number(stepInput.value);
+
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+
+  formEl.reset(); // Очищення форми
+}
+
+// Функція, що створює Promise
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
